df19f0097ba1004f7e48ff46827e8135458a1ced
Chawit.O <chawit.ocr@gmail.com>
2021-09-23 01:16:11 +0700

    url shortening function added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # url-shortener
-Just a practice project
+Just a practice project to make a URL shortener with React
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [longUrl, setLongUrl] = useState('')
+  const [shortUrl, setShortUrl] = useState('')
+  const config = { headers: { 'Authorization': 'Bearer 70143b81dd40a9b011c1462010fd99ccce308a7f' } }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('https://api-ssl.bitly.com/v4/shorten', { long_url: longUrl }, config )
+      .then(res => setShortUrl(res.data.id))
+  }
+  const copyToClipboard = () => { navigator.clipboard.writeText(shortUrl) }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>URL Shortener with React</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={longUrl} onChange={(e) => setLongUrl(e.target.value) } />
+          <input type="submit" value="Shorten URL" />
+        </form>
+        <p>
+          Original URL:
+          <a href={longUrl} target="_blank" rel="noopener noreferrer"> {longUrl}</a>
+        </p>
+        <div>{shortUrl}</div>
+        <button onClick={copyToClipboard}>Copy URL</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
