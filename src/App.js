11f27b05a85f06d8d01bae7b66d39542950c4560
Chawit.O <chawit.ocr@gmail.com>
2021-09-25 04:33:31 +0700

    styled the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
 import { useState } from 'react';
 import axios from 'axios';
@@ -17,22 +17,19 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>URL Shortener with React</p>
+        <h1>Link Shortener with React</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={longUrl} onChange={(e) => setLongUrl(e.target.value) } />
-          <input type="submit" value="Shorten URL" />
+          <input type="text" value={longUrl} placeholder="Paste the long url here" onChange={(e) => setLongUrl(e.target.value) } />
+          <input className="btn-url" type="submit" value="Shorten" />
         </form>
-        <p>
-          Original URL:
-          <a href={longUrl} target="_blank" rel="noopener noreferrer"> {longUrl}</a>
-        </p>
-        {shortUrl &&<div>
+        {shortUrl &&<div className="short-url-container">
           <div>{shortUrl}</div>
-          <button onClick={copyToClipboard}>Copy URL</button>
+          <button className="btn-url" onClick={copyToClipboard}>Copy</button>
         </div>}
-      </header>
+        {!shortUrl && <div className="short-url-placeholder"></div>}
+      </div>
     </div>
   );
 }
